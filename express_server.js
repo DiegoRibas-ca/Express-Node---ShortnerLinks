e640e6b19ae79aaa9880c9f133514820a1179f86
DiegoRibas-ca <diegoribas.ca@gmail.com>
2018-01-24 18:27:35 +0000

    Delete and Update shortURL implemented

--- express_server.js
+++ express_server.js
@@ -22,6 +22,10 @@ const urlDatabase ={
   "9sm5xK": "http://www.google.com",
 };
 
+app.get("/", function(req, res) {
+  res.end("Hello!");
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -33,24 +37,39 @@ app.post("/urls", (req, res) => {
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
-app.get("/", function(req, res) {
-  res.end("Hello!");
+//DELETE url - url_index template post buttom
+app.post("/urls/:id/delete", (req, res) => {
+  let urlDetele = req.params.id;
+  for (let lookKey in urlDatabase) {
+    if(lookKey == urlDetele) {
+      delete urlDatabase[lookKey];
+    };
+  };
+  res.redirect("/urls");
 });
-
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-})
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
 });
 
+//UPDATE ShortURL - urls_show template
+app.post("/urls/:id", (req, res) => {
+  let shortUpdate = req.params.id;
+  urlDatabase[shortUpdate] = req.body.longURL;
+  console.log(urlDatabase);  // debug statement to see POST parameters
+  res.redirect("/urls");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/u/:shortURL", (req, res) => {
-    let shortURL = req.params.shortURL
-    let longURL = urlDatabase[shortURL]
-    res.redirect(longURL);
+  let shortURL = req.params.shortURL
+  let longURL = urlDatabase[shortURL]
+  res.redirect(longURL);
 });
 
 
